e9403cb2659140a94cc4d17f5755377ca037b737
stefos79 <stef79@gmail.com>
2026-07-10 10:47:58 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,8 +9,8 @@
  * Όταν αλλάζεις κώδικα, ΑΛΛΑΞΕ το CACHE_VERSION για να φορτώσουν τα νέα αρχεία.
  */
 
-const CACHE_VERSION = 'nn-questionnaire-v19';
-const RUNTIME_CACHE = 'nn-runtime-v19';
+const CACHE_VERSION = 'nn-questionnaire-v20';
+const RUNTIME_CACHE = 'nn-runtime-v20';
 
 // Αρχεία που φορτώνουν αμέσως όταν εγκατασταθεί η εφαρμογή
 const APP_SHELL = [
